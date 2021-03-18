e7b2de14c2f4c93a7818bf9efcb7758ad9ba5fe2
Pankrashin-iqa14 <pankrashin.al@yandex.ru>
2021-03-18 20:29:34 +0400

    add all

---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,69 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.repository.ProductRepository;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class ProductManagerTest {
+    @Mock
+    private ProductRepository repository;
+
+    @InjectMocks
+    private ProductManager manager;
+
+    private Product prod1 = new Book(1, "Mumu", 200, "Turgenev");
+    private Product prod2 = new Book(2, "Idiot", 400, "Dostoevskiy");
+    private Product prod3 = new Book(3, "Revisor", 600, "Checkhov");
+    private Product prod4 = new Smartphone(4, "Galaxys20", 2500, "Samsung");
+    private Product prod5 = new Smartphone(5, "Redminote10", 1000, "Xiaomi");
+    private Product prod6 = new Smartphone(6, "Iphone12", 5000, "Apple");
+
+
+
+    @Test
+    public void shouldFindNameAuthor() {
+        Product[] returned = new Product[]{prod1, prod2, prod3, prod4, prod5, prod6};
+        doReturn(returned).when(repository).findAll();
+
+        Product[] actual = manager.searchBy("Checkhov");
+        Product[] expected = new Product[]{prod3};
+
+        assertArrayEquals(actual, expected);
+
+    }
+    @Test
+    public void shouldFindNameBook() {
+        Product[] returned = new Product[]{prod1, prod2, prod3, prod4, prod5, prod6};
+        doReturn(returned).when(repository).findAll();
+
+        Product[] actual = manager.searchBy("Idiot");
+        Product[] expected = new Product[]{prod2};
+
+        assertArrayEquals(actual, expected);
+
+    }
+    @Test
+    public void shouldFindNameSmartphone() {
+        Product[] returned = new Product[]{prod1, prod2, prod3, prod4, prod5, prod6};
+        doReturn(returned).when(repository).findAll();
+
+        Product[] actual = manager.searchBy("Redminote10");
+        Product[] expected = new Product[]{prod5};
+
+        assertArrayEquals(actual, expected);
+
+    }
+
+
+}
